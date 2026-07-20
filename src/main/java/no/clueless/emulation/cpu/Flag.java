57ec1f488cab6a4c54e2188a02b62dbad9618400
maritims <maritim@gmail.com>
2026-07-21 00:15:42 +0200

    Changed from hex to binary.

--- src/main/java/no/clueless/emulation/cpu/Flag.java
+++ src/main/java/no/clueless/emulation/cpu/Flag.java
@@ -1,14 +1,17 @@
 package no.clueless.emulation.cpu;
 
+/**
+ * Represents a flag in the status register of the MOS 6502 CPU.
+ */
 public enum Flag {
-    Negative(0x80),
-    Overflow(0x40),
-    Five(0x20),
-    Break(0x10),
-    Decimal(0x08),
-    InterruptDisable(0x04),
-    Zero(0x02),
-    Carry(0x01);
+    Negative(0b1000_0000),
+    Overflow(0b0100_0000),
+    Five(0b0010_0000),
+    Break(0b0001_0000),
+    Decimal(0b0000_1000),
+    InterruptDisable(0b0000_0100),
+    Zero(0b0000_0010),
+    Carry(0x0000_0001);
 
     private final int mask;
 
