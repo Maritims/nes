a72083ddbe26927872c5b9fec6299a1f83ef74b4
maritims <maritim@gmail.com>
2026-08-06 16:02:53 +0200

    Separation of concerns.

--- src/main/java/no/clueless/emulation/impl/ppu/NameTables.java
+++ src/main/java/no/clueless/emulation/impl/ppu/NameTables.java
@@ -1,71 +1,31 @@
 package no.clueless.emulation.impl.ppu;
 
 import static no.clueless.emulation.impl.Masks.MASK_12BIT;
-import static no.clueless.emulation.impl.PpuMemoryMap.NAME_TABLE_SIZE_MINUS_ONE;
 
 public class NameTables {
     private final int[][] tables = new int[2][1024];
 
-    private int readMirroredVertically(int address) {
-        if (address <= NAME_TABLE_SIZE_MINUS_ONE) {
-            return tables[0][address & NAME_TABLE_SIZE_MINUS_ONE];
-        }
-
-        if (address <= 0x07FF) {
-            return tables[1][address & NAME_TABLE_SIZE_MINUS_ONE];
-        }
-
-        if (address <= 0x0BFF) {
-            return tables[0][address & NAME_TABLE_SIZE_MINUS_ONE];
-        }
-
-        return tables[1][address & NAME_TABLE_SIZE_MINUS_ONE];
-    }
-
-    private int readNormally(int address) {
-        if (address <= NAME_TABLE_SIZE_MINUS_ONE) {
-            return tables[0][address & NAME_TABLE_SIZE_MINUS_ONE];
-        }
-
-        if (address <= 0x07FF) {
-            return tables[0][address & NAME_TABLE_SIZE_MINUS_ONE];
-        }
-
-        if (address <= 0x0BFF) {
-            return tables[1][address & NAME_TABLE_SIZE_MINUS_ONE];
-        }
-
-        return tables[1][address & NAME_TABLE_SIZE_MINUS_ONE];
+    /**
+     * Returns the nametable id based on the address and whether the nametable is mirrored vertically.
+     *
+     * @param address              The address to calculate the nametable id for.
+     * @param isMirroredVertically Whether the nametable is mirrored vertically.
+     * @return The nametable id by shifting and masking out the least singificant bit in the result. The address is shifted by 10 bits if mirrored vertically, otherwise by 11 bits.
+     */
+    public int getNameTableId(int address, boolean isMirroredVertically) {
+        address &= MASK_12BIT;
+        return isMirroredVertically ? ((address >> 10) & 1) : (address >> 11) & 1;
     }
 
     public int read(int address, boolean isMirroredVertically) {
         address &= MASK_12BIT;
-        return isMirroredVertically ? readMirroredVertically(address) : readNormally(address);
+        var nameTableId = getNameTableId(address, isMirroredVertically);
+        return tables[nameTableId][address & 0x03FF] & 0xFF;
     }
 
     public void write(int address, int data, boolean isMirroredVertically) {
         address &= MASK_12BIT;
-
-        if (isMirroredVertically) {
-            if (address <= NAME_TABLE_SIZE_MINUS_ONE) {
-                tables[0][address & NAME_TABLE_SIZE_MINUS_ONE] = data;
-            } else if (address <= 0x07FF) {
-                tables[1][address & NAME_TABLE_SIZE_MINUS_ONE] = data;
-            } else if (address <= 0x0BFF) {
-                tables[0][address & NAME_TABLE_SIZE_MINUS_ONE] = data;
-            } else {
-                tables[1][address & NAME_TABLE_SIZE_MINUS_ONE] = data;
-            }
-        } else {
-            if (address <= NAME_TABLE_SIZE_MINUS_ONE) {
-                tables[0][address & NAME_TABLE_SIZE_MINUS_ONE] = data;
-            } else if (address <= 0x07FF) {
-                tables[0][address & NAME_TABLE_SIZE_MINUS_ONE] = data;
-            } else if (address <= 0x0BFF) {
-                tables[1][address & NAME_TABLE_SIZE_MINUS_ONE] = data;
-            } else {
-                tables[1][address & NAME_TABLE_SIZE_MINUS_ONE] = data;
-            }
-        }
+        var nameTableId = getNameTableId(address, isMirroredVertically);
+        tables[nameTableId][address & 0x03FF] = data;
     }
 }
